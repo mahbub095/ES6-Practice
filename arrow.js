dc9f931da8f9855d6dc06c1eaba8844d5d289248
mahbub095 <mahbub.095@gmail.com>
2020-08-06 16:13:30 +0600

    Function Multiple array

--- arrow.js
+++ arrow.js
@@ -1,5 +1,24 @@
-function doubleIt(num){
-    return num*2;
+// function doubleIt(num){
+//     return num*2;
+// }
+
+
+// function declaration
+// const doubleIt = function(num){
+//     return num * 2; 
+// }
+
+const doubleIt = num => num *2;
+const add = (x,y) =>x+y;
+const give5 = ()=>5;
+const doMath = (x,y) =>{
+    const sum = x+y;
+    const diff = x- y;
+    const result = sum * diff;
+    return result;
 }
-const result = doubleIt(5);
-console.log(result);
+//const result = doubleIt(15);
+const result = add(70,70);
+const result2 = give5();
+const result3= doMath(7,5);
+console.log(result3);
